2532f9fdf1095b391e7775c423a8aab557537f05
Pedro Chaves Guedes <160842810+pedroguedes9@users.noreply.github.com>
2026-03-11 14:03:47 -0300

    criei as tabelas do banco de dados no model.py

--- projeto-confeitaria/app.py
+++ projeto-confeitaria/app.py
@@ -1,17 +1,48 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect
 from db import db
-
+from models import Users
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///confeitaria.db"
-db = SQLAlchemy()
 db.init_app(app)
 
 
-@app.route("/")
+@app.route("/",methods = ["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        telefone = request.form.get("telefone")
+        new_user = Users(username=username, email=email, password=password, telefone_number=telefone)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/")
+    else:
+        users = db.session.query(Users).all()
+        return render_template("index.html", users=users)
+
+@app.route("/delete", methods = ["GET", "POST"])
+def delete():
+    if request.method == "POST":
+        id = request.form.get("id")
+        user = db.session.query(Users).filter_by(id=id).first()
+        db.session.delete(user)
+        db.session.commit()
+        return redirect("/")
+
+@app.route("/change", methods = ["GET", "POST"])
+def change():
+    if request.method == "POST":
+        id = request.form.get("id")
+        new_name = request.form.get("new_name")
+        user = db.session.query(Users).filter_by(id=id).first()
+        user.username = new_name
+        db.session.commit()
+        return redirect("/")
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- projeto-confeitaria/models.py
+++ projeto-confeitaria/models.py
@@ -1,7 +1,103 @@
 from db import db
+from sqlalchemy import func
 
 class Users(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False, unique=True)
+    username = db.Column(db.String(30), nullable=False, unique=True )
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False) #255 para senha pois usa um hash
+    telefone_number = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
+
+    cart_items = db.relationship("CartItems", backref="user")
+    orders = db.relationship("Orders", backref="user")
+
+    def __init__(self, username:str, email:str, password:str, telefone_number:int):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.telefone_number = telefone_number
+
+
+class Products(db.Model):
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    description = db.Column(db.String, nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=True)
+    stock = db.Column(db.Integer, nullable=True)
+    is_active = db.Column(db.Integer, nullable=False, default=1)
+    image = db.Column(db.String, nullable=True)
+    created_at = db.Column(db.DateTime, default=func.now())
+
+    order_items = db.relationship("OrderItems", backref="product")
+
+    def __init__(self, name:str, price:float, description:str | None, category_id:int | None, stock:int, image:str | None ):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.category_id = category_id
+        self.stock = stock
+        self.image = image
+
+class Categories(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+
+    products = db.relationship("Products", backref="category")
+
+    def __init__(self, name:str):
+        self.name = name
+
+class CartItems(db.Model):
+    __tablename__ = "cart_items"
+    __table_args__ = (db.UniqueConstraint("user_id", "product_id"),)
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
+
+    def __init__(self,user_id:int, product_id:int, quantity:int):
+        self.user_id = user_id
+        self.product_id = product_id
+        self.quantity = quantity
+
+
+class Orders(db.Model):
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    status = db.Column(db.String(30) ,nullable=False)
+    total_price = db.Column(db.Numeric(10, 2), nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
+
+    items = db.relationship("OrderItems", backref="order")
+
+    def __init__(self, user_id:int, status:str, total_price:float ):
+        self.user_id = user_id
+        self.status = status
+        self.total_price = total_price
+
+class OrderItems(db.Model):
+    __tablename__ = "order_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, order_id: int, product_id:int, price:float, quantity:int):
+        self.order_id =order_id
+        self.product_id = product_id
+        self.price = price
+        self.quantity = quantity

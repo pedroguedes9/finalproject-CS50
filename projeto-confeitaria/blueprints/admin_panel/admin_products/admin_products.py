3a6e7f6d6a557794228c2cd62006392ecbb14f54
Pedro Chaves Guedes <160842810+pedroguedes9@users.noreply.github.com>
2026-04-29 21:23:56 -0300

    filtro na busca de produtos pública

--- projeto-confeitaria/blueprints/admin_panel/admin_products/admin_products.py
+++ projeto-confeitaria/blueprints/admin_panel/admin_products/admin_products.py
@@ -26,13 +26,74 @@ def products():
         .options(selectinload(Products.category))
         .order_by(Products.is_active.desc(), Products.id.asc())
     )
+
+    min_price_str = request.args.get("min-price", "").strip()
+    max_price_str = request.args.get("max-price", "").strip()
+
+    min_price_val = None
+    max_price_val = None
+
+    if min_price_str != "":
+        try:
+            min_price_val =  Decimal(min_price_str)
+        except InvalidOperation:
+            flash("Preço mínimo inválido", "error")
+            return redirect(url_for('admin_panel.admin_products.products'))
+        if min_price_val < 0:
+            flash("Preço mínimo inválido", "error")
+            return redirect(url_for('admin_panel.admin_products.products'))
+    
+    if max_price_str != "":
+        try:
+            max_price_val = Decimal(max_price_str)
+        except InvalidOperation:
+            flash("Preço máximo inválido", "error")
+            return redirect(url_for('admin_panel.admin_products.products'))
+        if max_price_val < 0:
+            flash("Preço máximo inválido", "error")
+            return redirect(url_for('admin_panel.admin_products.products'))
+
+    if min_price_val != None and max_price_val != None:
+        if min_price_val > max_price_val:
+            flash("O mínimo não pode ser maior que o máximo", "error")
+            return redirect(url_for('admin_panel.admin_products.products'))
+
+    if min_price_val != None:
+        base_query = base_query.filter(Products.price >= min_price_val)
+    if max_price_val != None:
+        base_query = base_query.filter(Products.price <= max_price_val)
+
+
+    product_name = request.args.get("product-name", "").strip().lower()
+    if product_name != "":
+        base_query = base_query.filter(
+            Products.name.ilike(f"%{product_name}%")
+        )
+
+
+    category_name = request.args.get("category-name", "").strip().lower()
+    if category_name != "":
+        base_query = base_query.filter(
+            Products.category.has(
+                Categories.name.ilike(f"%{category_name}%")
+            )
+        )
+
     products, total, total_pages, page = paginate_query(base_query, page, per_page)
     
+    categories = Categories.query.all()
+
     return render_template(
         "admin_products.html", 
         products=products,
         page=page,
-        total_pages=total_pages
+        total_pages=total_pages,
+        categories = categories,
+        min_price_val=min_price_val,
+        max_price_val = max_price_val,
+        product_name=product_name,
+        category_name=category_name,
+        current_category_name=category_name
     )
 
 @admin_products_bp.route("/create", methods = ["POST", "GET"])
